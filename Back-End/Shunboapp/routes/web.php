70b35fd73e903c048184544b92b1e2082d6f1668
BlackBird-911 <iftikharimrul@iut-dhaka.edu>
2022-04-17 14:05:06 +0600

    failed succession

--- Back-End/Shunboapp/routes/web.php
+++ Back-End/Shunboapp/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SslCommerzPaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::get('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::get('/success', [SslCommerzPaymentController::class, 'success']);
+Route::get('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::get('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::get('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
